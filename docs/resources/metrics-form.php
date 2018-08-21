377e27680acdfdc0468c0156b1e1335a75e0325e
Melissa Austin <maustin.mmdp@outlook.com>
2018-08-21 11:29:24 -0700

    Add test php form handler

---
+++ docs/resources/metrics-form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+  Employee ID: <?php echo $_POST["employeeID"]; ?><br>
+  Job Number: <?php echo $_POST["jobNumber"]; ?><br>
+  Hours Onsite: <?php echo $_POST["hoursOnsite"]; ?><br>
+  Miles Onsite: <?php echo $_POST["milesOnsite"]; ?><br>
+  SWA's: <?php echo $_POST["stopWorkAuthority"]; ?><br>
+  Haz ID's: <?php echo $_POST["hazID"]; ?><br>
+  BBO's: <?php echo $_POST["behaviorBasedObservation"]; ?><br>
+  Near Miss: <?php echo $_POST["nearMiss"]; ?><br>
+  Email: <?php echo $_POST["email"]; ?><br>
+  Message: <?php echo $_POST["message"]; ?>
+
+</body>
+
+</html>
