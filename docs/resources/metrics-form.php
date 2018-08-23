21ce4dec798245afdd0dac17fc3e6dfd616628f8
Melissa Austin <maustin.mmdp@outlook.com>
2018-08-23 07:47:11 -0700

    Update

--- docs/resources/metrics-form.php
+++ docs/resources/metrics-form.php
@@ -1,18 +1,36 @@
+<?php
+  if(isset($_POST['metrics-form'])) {
+    $employeeID = $_POST['employeeID'];
+    $jobNumber = $_POST['jobNumber'];
+    $hoursOnsite = $_POST['hoursOnsite'];
+    $milesOnsite = $_POST['milesOnsite'];
+    $stopWorkAuthority = $_POST['stopWorkAuthority'];
+    $hazID = $_POST['hazID'];
+    $behaviorBasedObservation = $_POST['behaviorBasedObservation'];
+    $nearMiss = $_POST['nearMiss'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $formContent = "Employee ID: $employeeID \n Job Number: $jobNumber \n Hours Onsite: $hoursOnsite \n Miles Onsite: $milesOnsite \n SWA's: $stopWorkAuthority \n Haz ID's: $hazID \n BBO's: $behaviorBasedObservation \n Near Miss: $nearMiss \n Email: $email \n Message: $message";
+    $recipient = "melissa.mmdp@gmail.com";
+    $subject = "Metrics Form";
+    $headers = "From: $email";
+    mail($recipient, $subject, $formContent, $headers) or die("Error!");
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
+<head>
+	<meta charset="UTF-8">
+	<title>php test</title>
+  <link rel="stylesheet" href="php-style.css">
+</head>
+
 <body>
 
-  Employee ID: <?php echo $_POST["employeeID"]; ?><br>
-  Job Number: <?php echo $_POST["jobNumber"]; ?><br>
-  Hours Onsite: <?php echo $_POST["hoursOnsite"]; ?><br>
-  Miles Onsite: <?php echo $_POST["milesOnsite"]; ?><br>
-  SWA's: <?php echo $_POST["stopWorkAuthority"]; ?><br>
-  Haz ID's: <?php echo $_POST["hazID"]; ?><br>
-  BBO's: <?php echo $_POST["behaviorBasedObservation"]; ?><br>
-  Near Miss: <?php echo $_POST["nearMiss"]; ?><br>
-  Email: <?php echo $_POST["email"]; ?><br>
-  Message: <?php echo $_POST["message"]; ?>
+<h1>Thank You!</h1>
+<p>Your form has been sent.</p>
 
 </body>
 
