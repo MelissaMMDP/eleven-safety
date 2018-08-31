f1654f4d6a20a84adcf414519259b5a9cd9b262c
Melissa Austin <maustin.mmdp@outlook.com>
2018-08-31 10:40:50 -0700

    Remove line and move variable

--- docs/index.php
+++ docs/index.php
@@ -29,9 +29,9 @@
 				$headers = "From: $email";
 		    $subject = "Metrics Form";
 		    $recipient = "your-email@somewhere.com"; // update to valid email or $formData will throw an undefined variable error
+				$formData = "Employee ID: $employeeID \n Job Number: $jobNumber \n Hours Onsite: $hoursOnsite \n Miles Onsite: $milesOnsite \n SWA's: $stopWorkAuthority \n Haz ID's: $hazID \n BBO's: $behaviorBasedObservation \n Near Miss: $nearMiss \n Email: $email \n Message: $message";
 		    $formDataThankyou = "<h3>Here is the form data you submitted:</h3> <br>" . "<p>Employee ID: $employeeID <br> Job Number: $jobNumber <br> Hours Onsite: $hoursOnsite <br> Miles Onsite: $milesOnsite <br> SWA's: $stopWorkAuthority <br> Haz ID's: $hazID <br> BBO's: $behaviorBasedObservation <br> Near Miss: $nearMiss <br> Email: $email <br> Message: $message</p>";
 				mail($recipient, $subject, $formData, $headers) or die("Error!");
-				$formData = "Employee ID: $employeeID \n Job Number: $jobNumber \n Hours Onsite: $hoursOnsite \n Miles Onsite: $milesOnsite \n SWA's: $stopWorkAuthority \n Haz ID's: $hazID \n BBO's: $behaviorBasedObservation \n Near Miss: $nearMiss \n Email: $email \n Message: $message";
 
 				include 'resources/form-success.php';
 		    } else
